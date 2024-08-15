9060ea68303e37bc5bc0cbb8b2d8a6792227777b
markbuckle <markbuckle92@gmail.com>
2024-08-15 08:38:26 -0230

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,8 @@ export default function RootLayout({
     // provider will pass the data onto other clerk components like sign out, sign in and user button
     <ClerkProvider>
       <html lang="en">
-        <Head>{children}</Head>
         <body>
-          <Script src="https://saas-widget-2.vercel.app//widget.umd.js"></Script>
+          <Script src="https://saas-widget.vercel.app//widget.umd.js"></Script>
           <PageHeader />
           <my-widget></my-widget>
           {children}
