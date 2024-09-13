584a6db862044b8b1b58f06d0dda3be6fc7d4a42
Mark Buckle <markbuckle92@gmail.com>
2024-09-13 10:37:23 -0230

    typo fix

--- app/(user)/projects/[projectId]/page.tsx
+++ app/(user)/projects/[projectId]/page.tsx
@@ -43,7 +43,7 @@ const page = async ({
         <div className="flex flex-col">
           {project.url ? (
             <Link
-              href={"https://saas-dashboard.vercel.app"}
+              href={"https://saasdashboard.vercel.app"}
               className="underline text-indigo-700 flex items-center"
             >
               <Globe className="h-5 w-5 mr-2" />
